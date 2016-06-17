3803e85d42c8d4aeee5706fe691c31aed5a6ae2b
sweetlyn <arlenebudoso@gmail.com>
2016-06-17 08:14:36 +0800

    Update Comparison.java

--- Comparison.java
+++ Comparison.java
@@ -5,15 +5,15 @@ public class Comparison
     public static void main( String args[] )    
     {
         // create Scanner to obtain input from command window       
-        Scanner input = Scanner( System.in );  
+        Scanner input = new Scanner( System.in );  
         int number1; // first number to compare       
         int number2; // second number to compare  
       
-        System.out.prnt( "Enter first integer: " ); // prompt        
+        System.out.print( "Enter first integer: " ); // prompt        
         number1 = input.nextInt(); // read first number from user   
  
         System.out.print( "Enter second integer: " ); // prompt        
-        num2 = input.nextInt(); // read second number from user               
+        number2 = input.nextInt(); // read second number from user               
         
         if ( number1 == number2 )           
             Systm.out.printf( "%d == %d\n", number1, number2 );  
@@ -27,6 +27,6 @@ public class Comparison
             System.out.printf( "%d <= %d\n", number1, number2 );  
         if ( number1 >= number2 )          
             System.out.printf( "%d >= %d\n", number1, number2 );  
-    }
+    
    } // end method main  
 } // end class Comparison 
